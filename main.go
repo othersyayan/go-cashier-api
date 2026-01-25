0d525196a93b57791a33745ffb27758e40bf4c0a
Yayan Handani <others.handani@gmail.com>
2026-01-25 17:46:26 +0700

    [main] feat: Implement initial product management API with CRUD operations and in-memory storage for a cashier system.

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-cashier-api/internal/delivery/http"
+	"go-cashier-api/internal/repository"
+	"go-cashier-api/internal/usecase"
+	netHttp "net/http"
+)
+
+func main() {
+	repo := repository.NewInMemoryProductRepository()
+	productUsecase := usecase.NewProductUsecase(repo)
+	productHandler := http.NewProductHandler(productUsecase)
+
+	productHandler.RegisterRoutes()
+
+	netHttp.HandleFunc("/health", func(w netHttp.ResponseWriter, r *netHttp.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "OK",
+			"message": "API Running",
+		})
+	})
+
+	fmt.Println("Server running di localhost:8080")
+
+	err := netHttp.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("gagal running server")
+	}
+}
