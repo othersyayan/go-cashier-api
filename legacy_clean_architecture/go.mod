a6b84a2656ecd4e7b4cb35f1bdb774109eceb7fc
Yayan Handani <others.handani@gmail.com>
2026-02-01 18:54:48 +0700

    [main] refactor: Remove the entire legacy clean architecture module.

--- legacy_clean_architecture/go.mod
+++
@@ -1,3 +0,0 @@
-module go-cashier-api
-
-go 1.25.6
--- legacy_clean_architecture/internal/entity/category.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Category struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
